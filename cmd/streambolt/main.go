b3bb4f3313be667cc112443c3f2baf7a6cdc1792
Scott Clasen <scott@heroku.com>
2015-09-19 19:21:58 -0700

    more cli

--- cmd/streambolt/main.go
+++ cmd/streambolt/main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/boltdb/bolt"
 	"github.com/codegangsta/cli"
 	"os"
+	"strings"
 )
 
 /*
 streambolt count bucket file
-streambolt list  bucket startInclusivePrefix endExclusivePrefix file
+streambolt keys  bucket prefix file
+streambolt list  bucket prefix file
 streambolt get   bucket key file
 streambolt all key bucket bucket file
 */
@@ -17,6 +20,11 @@ streambolt all key bucket bucket file
 var bucket = cli.StringFlag{
 	Name:  "bucket, b",
 	Usage: "bucket to inspect",
+}
+
+var buckets = cli.StringFlag{
+	Name:  "buckets, bs",
+	Usage: "comma seperated buckets to inspect",
 }
 
 var key = cli.StringFlag{
@@ -27,6 +35,7 @@ var key = cli.StringFlag{
 var db = cli.StringFlag{
 	Name:  "db, d",
 	Usage: ".boltdb file to inspect",
+	EnvVar: "BOLTDB",
 }
 
 var start = cli.StringFlag{
@@ -37,6 +46,11 @@ var start = cli.StringFlag{
 var end = cli.StringFlag{
 	Name:  "end, e",
 	Usage: "exclusive end key range",
+}
+
+var prefix = cli.StringFlag{
+	Name:  "prefix, p",
+	Usage: "key prefix",
 }
 
 func main() {
@@ -52,25 +66,32 @@ func main() {
 			Flags:   []cli.Flag{bucket, db},
 		},
 		{
+			Name:    "keys",
+			Aliases: []string{"k"},
+			Usage:   "show the keys (for a key prefix) in a bucket",
+			Action:  Keys,
+			Flags:   []cli.Flag{bucket, db, prefix},
+		},
+		{
 			Name:    "list",
 			Aliases: []string{"l"},
-			Usage:   "show the values for a key range in a bucket",
-			Action:  Count,
-			Flags:   []cli.Flag{bucket, db, start, end},
+			Usage:   "show the values (for a key prefix) in a bucket",
+			Action:  List,
+			Flags:   []cli.Flag{bucket, db, prefix},
 		},
 		{
 			Name:    "get",
 			Aliases: []string{"g"},
 			Usage:   "get the value for a key in a bucket",
-			Action:  Count,
+			Action:  Get,
 			Flags:   []cli.Flag{bucket, db, key},
 		},
 		{
 			Name:    "all",
 			Aliases: []string{"a"},
-			Usage:   "get the values for a key in many buckets",
-			Action:  Count,
-			Flags:   []cli.Flag{bucket, db, key},
+			Usage:   "get the values for a key in many buckets, first bucket is the keylist",
+			Action:  All,
+			Flags:   []cli.Flag{buckets, db, key},
 		},
 	}
 	app.Run(os.Args)
@@ -94,20 +115,111 @@ func withBolt(c *cli.Context, view func(*bolt.Tx) error) {
 	}
 }
 
-func Count(c *cli.Context) {
+func withBucket(c *cli.Context, view func(string, *bolt.Tx) error) {
 	b := c.String("bucket")
 	if b == "" {
 		println("No --bucket")
 	} else {
 		withBolt(c, func(tx *bolt.Tx) error {
-			count := 0
-			bb := tx.Bucket([]byte(b))
-			bb.ForEach(func(k, v []byte) error {
-				count++
-				return nil
-			})
-			fmt.Printf("bucket: %s key-count: %d\n", b, count)
-			return nil
+			return view(b, tx)
 		})
 	}
+}
+
+func withBuckets(c *cli.Context, view func([]string, *bolt.Tx) error) {
+	b := c.String("buckets")
+	if b == "" {
+		println("No --buckets")
+	} else {
+		withBolt(c, func(tx *bolt.Tx) error {
+			return view(strings.Split(b,","), tx)
+		})
+	}
+}
+
+func Count(c *cli.Context) {
+	withBucket(c, func(bucket string, tx *bolt.Tx) error {
+		count := 0
+		bb := tx.Bucket([]byte(bucket))
+		bb.ForEach(func(k, v []byte) error {
+			count++
+			return nil
+		})
+		fmt.Printf("bucket=%s key-count=%d\n", bucket, count)
+		return nil
+	})
+}
+
+func Keys(c *cli.Context) {
+	withBucket(c, func(bucket string, tx *bolt.Tx) error {
+		bb := tx.Bucket([]byte(bucket))
+		cu := bb.Cursor()
+		if p := c.String("prefix"); p != "" {
+			for k, _ := cu.Seek(b(p)); bytes.HasPrefix(k, b(p)); k, _ = cu.Next() {
+				fmt.Println(s(k))
+			}
+		} else {
+			for k, _ := cu.First(); k != nil; k, _ = cu.Next() {
+				fmt.Println(s(k))
+			}
+		}
+		return nil
+	})
+}
+
+func Get(c *cli.Context) {
+	withBucket(c, func(bucket string, tx *bolt.Tx) error {
+		bb := tx.Bucket([]byte(bucket))
+		if key := c.String("key"); key != "" {
+			fmt.Println(s(bb.Get(b(key))))
+		}
+		return nil
+	})
+}
+
+func List(c *cli.Context) {
+	withBucket(c, func(bucket string, tx *bolt.Tx) error {
+		bb := tx.Bucket([]byte(bucket))
+		cu := bb.Cursor()
+		if p := c.String("prefix"); p != "" {
+			for k, v := cu.Seek(b(p)); bytes.HasPrefix(k, b(p)); k, v = cu.Next() {
+				fmt.Printf("key=%s value=%s\n", s(k), s(v))
+			}
+		} else {
+			for k, v := cu.First(); k != nil; k, v = cu.Next() {
+				fmt.Printf("key=%s value=%s\n", s(k), s(v))
+			}
+		}
+		return nil
+	})
+}
+
+func All(c *cli.Context) {
+	withBuckets(c, func(buckets []string, tx *bolt.Tx) error {
+		if k := c.String("key"); k != "" {
+			bucketMap := map[string]*bolt.Bucket{}
+			for _, ab := range buckets {
+				bucketMap[ab] = tx.Bucket(b(ab))
+			}
+			fmt.Println(others(bucketMap, b(k)))
+		}
+		return nil
+	})
+}
+
+func others(o map[string]*bolt.Bucket, key []byte) string {
+	ret := ""
+	for k, b := range o {
+		v := b.Get(key)
+		ret = ret + fmt.Sprintf("%s=%s", k, s(v))
+	}
+	return ret
+}
+
+func b(s string) []byte {
+	return []byte(s)
+}
+
+func s(b []byte) string {
+	return string(b)
 }
